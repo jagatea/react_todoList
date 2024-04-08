0dc4ec3b08f112527d7096a58ffcf29727d3d956
ACHIMO <57163749+jagatea@users.noreply.github.com>
2024-04-08 17:08:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-    ? 'react_todoList'
-    : './',
+  base: "./",
   plugins: [
     react(),
     VitePWA({
