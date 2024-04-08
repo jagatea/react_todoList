435d1bed814f51499cf2e83563b6193d8c4ad7c9
ACHIMO <57163749+jagatea@users.noreply.github.com>
2024-04-08 17:18:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,16 +10,17 @@ export default defineConfig({
       registerType: "autoUpdate",
       manifest: {
         // ユーザーに通常表示されるアプリ名
-        name: 'Todo App (PWA)',
+        name: "Todo App (PWA)",
         // name を表示するのに十分なスペースがない場合に表示されるアプリ名
-        short_name: 'Todo',
+        short_name: "Todo",
         // アプリの詳細な説明
-        description: 'Todo プログレッシブ・ウェブアプリ',
+        description: "Todo プログレッシブ・ウェブアプリ",
+        id: "/react_todoList/",
         /**
          * アプリの開始 URL:
          * 通常はサーブするディレクトリそのもの
          */
-        start_url: '.',
+        start_url: ".",
         /**
          * 表示モード:
          * fullscreen: フルスクリーン
@@ -36,7 +37,7 @@ export default defineConfig({
          */
         orientation: "portrait",
         // 既定のテーマカラー
-        theme_color: '#3f51b2',
+        theme_color: "#3f51b2",
         // スタイルシートが読み込まれる前に表示するアプリページの背景色
         background_color: "#efeff4",
         /**
@@ -45,21 +46,20 @@ export default defineConfig({
          */
         icons: [
           {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'icon-512x512-mask.png',
-            sizes: '512x512',
-            type: 'image/png',
-            // 用途をマスカブルアイコンとする
-            purpose: 'maskable',
+            src: "icon-512x512-mask.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       }
